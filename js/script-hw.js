386b33a22404543019d3737bd77e121ae32189f0
Evgeniya3010 <susskaya86@gmail.com>
2020-10-07 18:41:36 +0300

    practice

--- js/script-hw.js
+++ js/script-hw.js
@@ -1,52 +1,52 @@
 "use strict";
 
-function isLoginValid(login, min = 4, max = 16) {
-  // Write code under this line
-  const loginValid = login.length >= min && login.length <= max;
-  return loginValid;
-  // return login.length >= 4 && login.length <= 16 ? "true" : "false";
-}
+// function isLoginValid(login, min = 4, max = 16) {
+//   // Write code under this line
+//   const loginValid = login.length >= min && login.length <= max;
+//   return loginValid;
+//   // return login.length >= 4 && login.length <= 16 ? "true" : "false";
+// }
 
-function isLoginUnique(allLogins, login) {
-  "use strict";
-  // Write code under this line
-  // return allLogins.includes(login) ? "false" : "true";
-  const loginUnique = allLogins.includes(login);
-  if (loginUnique === true) {
-    return false;
-  }
-  return true;
-}
+// function isLoginUnique(allLogins, login) {
+//   "use strict";
+//   // Write code under this line
+//   // return allLogins.includes(login) ? "false" : "true";
+//   const loginUnique = allLogins.includes(login);
+//   if (loginUnique === true) {
+//     return false;
+//   }
+//   return true;
+// }
 
-function addLogin(allLogins, login) {
-  "use strict";
-  const SUCCESS = "Логин успешно добавлен!";
-  const REFUSAL = "Такой логин уже используется!";
-  const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
-  let message;
-  // Write code under this line
-  if (isLoginValid(login) === false) {
-    message = ERROR;
-  } else if (isLoginUnique(allLogins, login) === false) {
-    message = REFUSAL;
-  } else if (isLoginUnique(allLogins, login) === true) {
-    allLogins.push(login);
-    message = SUCCESS;
-  }
-  return message;
-}
-const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+// function addLogin(allLogins, login) {
+//   "use strict";
+//   const SUCCESS = "Логин успешно добавлен!";
+//   const REFUSAL = "Такой логин уже используется!";
+//   const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
+//   let message;
+//   // Write code under this line
+//   if (isLoginValid(login) === false) {
+//     message = ERROR;
+//   } else if (isLoginUnique(allLogins, login) === false) {
+//     message = REFUSAL;
+//   } else if (isLoginUnique(allLogins, login) === true) {
+//     allLogins.push(login);
+//     message = SUCCESS;
+//   }
+//   return message;
+// }
+// const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-console.log(addLogin(logins, "Ajax"));
-// 'Логин успешно добавлен!'
+// console.log(addLogin(logins, "Ajax"));
+// // 'Логин успешно добавлен!'
 
-console.log(addLogin(logins, "robotGoogles"));
-// 'Такой логин уже используется!'
+// console.log(addLogin(logins, "robotGoogles"));
+// // 'Такой логин уже используется!'
 
-console.log(addLogin(logins, "Zod"));
-// 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, "Zod"));
+// // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
-console.log(addLogin(logins, "jqueryisextremelyfast"));
+// console.log(addLogin(logins, "jqueryisextremelyfast"));
 // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
 // создаем массив из аргументов функции
