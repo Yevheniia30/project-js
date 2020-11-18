525c223c9f991eab5fdb106129b5fea2799e747d
Evgeniya3010 <susskaya86@gmail.com>
2020-11-18 06:10:35 +0200

    practice

--- js/module-07.js
+++ js/module-07.js
@@ -1,6 +1,59 @@
 // DOM
 // СОБЫТИЯ
 
+// ПРИМЕР 1
+// const inputRef = document.querySelector(".name");
+// console.log(inputRef);
+// const input2Ref = document.querySelector(".surname");
+// console.log(input2Ref);
+// const input3Ref = document.querySelector(".check");
+// console.log(input3Ref);
+// const buttonRef = document.querySelector(".js-button");
+// console.log(buttonRef);
+// const spanRef = document.querySelector(".username");
+// console.log(spanRef);
+// const textRef = document.querySelector(".text");
+// console.log(textRef);
+
+// buttonRef.setAttribute("disabled", true);
+
+// inputRef.addEventListener("change", (event) => {
+//   textRef.textContent = event.target.value;
+// });
+
+// const handleInputFocus = (event) => {
+//   event.preventDefault();
+//   console.log("Got focus");
+// };
+// const handleInputBlur = (event) => {
+//   console.log("Lost focus");
+// };
+// const handleInputChange = (event) => {
+//   spanRef.textContent = event.target.value;
+// };
+// const handleCheckChange = (event) => {
+//   event.preventDefault();
+//   buttonRef.disabled = !event.target.checked;
+// };
+
+// inputRef.addEventListener("focus", handleInputFocus);
+// inputRef.addEventListener("blur", handleInputBlur);
+// input2Ref.addEventListener("input", handleInputChange);
+// input3Ref.addEventListener("change", handleCheckChange);
+
+// ПРИМЕР 2
+const formRef = document.querySelector("form");
+console.log(formRef);
+
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target.elements.login.value);
+  console.log(event.target.elements.password.value);
+});
+
+const data = new FormData(formRef);
+console.log(data);
+// data.forEach((item) => console.log(item));
 
 // const titleEl = document.querySelector('.main-title');
 // console.dir(titleEl);
@@ -10,11 +63,9 @@
 // titleEl.style.color = 'tomato';
 // titleEl.style.fontSize = '40px';
 
-
 // const listEl = document.querySelector('.main-list-item');
 // console.log(listEl);
 // console.log(listEl.classList);
-
 
 // const subTitleEl = document.querySelector('.sub-title');
 // console.log(subTitleEl);
@@ -25,7 +76,6 @@
 
 // imageEl.alt = "It's me";
 // imageEl.src='https://cdn.pixabay.com/photo/2013/05/30/18/21/cat-114782_960_720.jpg'
-
 
 // const heading = document.createElement('h1');
 // console.log(heading); // <h1></h1>
