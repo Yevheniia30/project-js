5019e27336e0a08c6cc2850eed5fc4359423d72f
Evgeniya3010 <susskaya86@gmail.com>
2020-11-20 01:19:06 +0200

    пример

---
+++ js/module-08.js
@@ -0,0 +1,22 @@
+// ПРИМЕР 1
+const menu = document.querySelector(".js-nav");
+console.log(menu);
+
+const menuClickHandler = (event) => {
+  const current = event.target;
+  //
+  const prev = document.querySelector(".active");
+  // target - это элемент на которым мы кликнули, currentTarget - элемен на котором стоит eventListener
+  if (event.currentTarget === event.target) {
+    return;
+  }
+  // снимаем с элемента с существующим классом active класс и вешаем его на текущий элемент
+  if (prev) {
+    prev.classList.remove("active");
+  }
+  current.classList.add("active");
+  console.log(current);
+  console.log(prev);
+};
+
+menu.addEventListener("click", menuClickHandler);
